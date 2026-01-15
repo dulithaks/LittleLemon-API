94d33bf4596e7b971552c3ecf09e06f523973c31
Dulitha <dulithaks@gmail.com>
2026-01-15 12:51:33 +0530

    Disable trailing slash

--- LittleLemonDRF/urls.py
+++ LittleLemonDRF/urls.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter 
 from .views import MenuItemViewSet
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
 router.register(r'menu-items', MenuItemViewSet, basename='menu-items')
 
 urlpatterns = router.urls
