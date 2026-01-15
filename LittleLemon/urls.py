64e2ffce3f05745070eabb01406edcf62937e554
Dulitha <dulithaks@gmail.com>
2026-01-15 11:04:55 +0530

    url

--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('LittleLemonDRF.urls')), 
     path('api/', include('djoser.urls')), 
-    path('api/', include('djoser.urls.authtoken')), 
+    path('', include('djoser.urls.authtoken')), 
 ]
